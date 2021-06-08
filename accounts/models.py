e241be77cfb151eacb1b2b9975d9b44fd8fdff92
comanuel7@gmail.com <comanuel7@gmail.com>
2021-06-07 20:36:34 -0500

    django authentication

--- accounts/models.py
+++ accounts/models.py
@@ -3,36 +3,38 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 # Create your models here.
 
 class MyAccountManager(BaseUserManager):
-    def create_user(self, first_name,last_name,username,email, password=None):
+    def create_user(self, first_name, last_name, username, email, password=None):
         if not email:
-            raise ValueError('El usuario debe ser una dirección de correo')
+            raise ValueError('User must have an email address')
+
         if not username:
-            raise ValueError('Usuario debe ser un nombre de usuario')
-        
+            raise ValueError('User must have an username')
+
         user = self.model(
             email = self.normalize_email(email),
             username = username,
-            first_name=first_name,
-            last_name=last_name,
-        )    
-        
+            first_name = first_name,
+            last_name = last_name,
+        )
+
         user.set_password(password)
         user.save(using=self._db)
         return user
     
     def create_superuser(self, first_name, last_name, email, username, password):
-        user=self.create_user(
+        user = self.create_user(
             email = self.normalize_email(email),
-            username=username,
-            password=password,
-            first_name=first_name,
-            last_name=last_name,
+            username = username,
+            password = password,
+            first_name = first_name,
+            last_name = last_name,
         )
-        user.is_admin=True
-        user.is_active=True
-        user.is_staff=True
-        user.is_superadmin=True
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superadmin = True
         user.save(using=self._db)
+        return user
         
 
 
@@ -55,7 +57,7 @@ class Account(AbstractBaseUser):
     REQUIRED_FIELDS = ['username','first_name','last_name']
     
     
-    object = MyAccountManager()
+    objects = MyAccountManager()
     def __str__(self):
         return self.email
     
--- greatkart/settings.py
+++ greatkart/settings.py
@@ -132,3 +132,17 @@ STATICFILES_DIRS = [
 # media files configuration
 MEDIA_URL='/media/'
 MEDIA_ROOT= BASE_DIR /'media'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+#CONFIGURACION SMTP
+EMAIL_HOST= 'smtp.gmail.com'
+EMAIL_PORT= '587'
+EMAIL_HOST_USER= 'comanuel7@gmail.com'
+EMAIL_HOST_PASSWORD='wmxnznostkuoonyw'
+EMAIL_USE_TLS= True
